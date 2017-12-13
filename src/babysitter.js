1f0b5ad566a4765af0c5f4d57e0df679492de6b3
alliestehney <alliestehney1@gmail.com>
2017-12-12 22:16:46 -0500

    Finished test for too late of an end time

--- src/babysitter.js
+++ src/babysitter.js
@@ -2,8 +2,16 @@ function checkStart(start) {
 	return start >= 17;
 }
 
+function checkEnd(end) {
+	if (end > 4) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
 function calculateHours(start, end, bedtime) {
-	if (checkStart(start) === true) {
+	if (checkStart(start) === true && checkEnd(end) === true) {
 		return 96;
 	} else {
 		return false;
--- test/babysitter.test.js
+++ test/babysitter.test.js
@@ -8,4 +8,8 @@ describe("babysitter function", function() {
 	it("babysitter starts no earlier than 5pm", function() {
 		expect(calculateHours(16,2,19)).toBe(false);
 	});
+
+	it("babysitter leaves no later than 4am", function() {
+		expect(calculateHours(17,5,19)).toBe(false);
+	});
 });
