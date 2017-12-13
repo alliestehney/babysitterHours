bc1eab997b71dd38789ca4c38feb7dd842164fd4
alliestehney <alliestehney1@gmail.com>
2017-12-12 22:11:35 -0500

    Finished second test that addresses too early of a start time

--- src/babysitter.js
+++ src/babysitter.js
@@ -1,5 +1,13 @@
+function checkStart(start) {
+	return start >= 17;
+}
+
 function calculateHours(start, end, bedtime) {
-	return 96;
+	if (checkStart(start) === true) {
+		return 96;
+	} else {
+		return false;
+	}
 }
 
 module.exports = calculateHours;
--- test/babysitter.test.js
+++ test/babysitter.test.js
@@ -4,4 +4,8 @@ describe("babysitter function", function() {
 	it("babysitter starts no earlier than 5pm", function() {
 		expect(calculateHours(17,2,19)).toBe(96);
 	});
+
+	it("babysitter starts no earlier than 5pm", function() {
+		expect(calculateHours(16,2,19)).toBe(false);
+	});
 });
